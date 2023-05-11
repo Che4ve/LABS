ffa1575021b6f3e4747703527946c054c6b8b443
Che4ve <chesnokov_vd@mail.ru>
2023-05-11 19:41:36 +0300

    Added dynamic memory allocation for the vector

--- KP-7/spmatrix.c
+++ KP-7/spmatrix.c
@@ -3,7 +3,8 @@
 sparseMatrix* initSparseMatrix(void)
 {
     sparseMatrix* matrix = malloc( sizeof(sparseMatrix) );
-    matrix->vector = calloc( MAX_VECTOR_SIZE, sizeof(vectorElement) );
+    matrix->vectorAllocatedSize = VECTOR_DEFAULT_SIZE;
+    matrix->vector = calloc( matrix->vectorAllocatedSize, sizeof(vectorElement) );
     matrix->rows = 0;
     matrix->cols = 0;
     matrix->vectorSize = 0;
@@ -13,12 +14,16 @@ sparseMatrix* initSparseMatrix(void)
 sparseMatrix* initSparseIdentityMatrix(int order)
 {
     sparseMatrix* matrix = malloc( sizeof(sparseMatrix) );
-    matrix->vector = calloc( MAX_VECTOR_SIZE, sizeof(vectorElement) );
+    matrix->vectorAllocatedSize = VECTOR_DEFAULT_SIZE;
+    matrix->vector = calloc( matrix->vectorAllocatedSize, sizeof(vectorElement) );
     matrix->rows = order;
     matrix->cols = order;
     matrix->vectorSize = 0;
     for (int i = 1; i <= order; i++) {
-        pushBack(matrix, i, i, 1.0);
+        if ( pushBack(matrix, i, i, 1.0) == -1 ) {
+            freeSparseMatrix(matrix);
+            return NULL;
+        };
     }
     matrix->vector[matrix->vectorSize - 1] = NilElem;
     return matrix;
@@ -26,12 +31,22 @@ sparseMatrix* initSparseIdentityMatrix(int order)
 
 int pushBack(sparseMatrix* matrix, int row, int col, double data)
 {
-    if (data == 0 && matrix->cols != col) {
+    if ( matrix->vectorSize == VECTOR_MAX_ELEMENTS ) {
+        return -1;
+    }
+    if ( data == 0 && matrix->cols != col ) {
         return 0;
     }
 
     vectorElement* vector = matrix->vector;
-    if (memcmp(&vector[0], &NilElem, sizeof(vectorElement)) == 0) {
+
+    // Realloc if reached maximum size
+    if ( matrix->vectorSize + 1 >= matrix->vectorAllocatedSize ) {
+        matrix->vectorAllocatedSize *= 2;
+        matrix->vector = realloc(matrix->vector, matrix->vectorAllocatedSize * sizeof(vectorElement));
+    }
+
+    if ( memcmp(&vector[0], &NilElem, sizeof(vectorElement) ) == 0) {
         // prevElement is equal to NilElem
         vectorElement initElement = { 0, { .row = 1 } };
         matrix->vector[0] = initElement;
@@ -44,12 +59,12 @@ int pushBack(sparseMatrix* matrix, int row, int col, double data)
     vectorElement newElement     = { col, { .value  = data } }; 
     vectorElement closureElement = {   0, { .row = row + 1 } };
 
-    if (data == 0 && matrix->cols == col) {
+    if ( data == 0 && matrix->cols == col ) {
         vector[index - 1] = closureElement;
         return 1;
     }
 
-    if (prevElement.qualifier == 0 && prevElement.data.row == row) {
+    if ( prevElement.qualifier == 0 && prevElement.data.row == row ) {
         vector[index] = newElement;
         matrix->vectorSize++;
         vector[index + 1] = closureElement;
@@ -146,7 +161,7 @@ int readSparseMatrix(sparseMatrix* matrix, FILE* stream)
         return -1;
     }
     if (feof(stream)) {
-        return 0;
+        return -2;
     }
     int rows, cols;
     if (stream == stdin) {
@@ -159,25 +174,29 @@ int readSparseMatrix(sparseMatrix* matrix, FILE* stream)
     if (stream == stdin) {
         printf("Enter the matrix in a standard format: \n");
     }
-    
     matrix->rows = rows;
     matrix->cols = cols;
-
+    int pushRet = 0;
     for (int i = 1; i <= rows; i++) {
         for (int j = 1; j <= cols; j++) {
             double value;
             fscanf(stream, "%lf", &value);
-            pushBack(matrix, i, j, value);
+            pushRet = pushBack(matrix, i, j, value);
         }
     }
+    if (pushRet == -1) {
+        fprintf(stderr, "Error: Vector '%p' has reached element limit.\n", matrix->vector);
+        return -3;
+    }
     matrix->vector[matrix->vectorSize - 1] = NilElem;
-    matrix->vector = realloc(matrix->vector, matrix->vectorSize * sizeof(vectorElement));
+    matrix->vectorAllocatedSize = matrix->vectorSize;
+    matrix->vector = realloc(matrix->vector, matrix->vectorAllocatedSize * sizeof(vectorElement));
     return 1;
 }
 
 int printSparseMatrix(sparseMatrix* matrix)
 {
-    for (int i = 0; i < MAX_VECTOR_SIZE; i++) {
+    for (int i = 0; i < VECTOR_DEFAULT_SIZE; i++) {
         vectorElement elem = matrix->vector[i];
         if (elem.qualifier == 0) {
             // the data is an int (stored in the `column` field)
